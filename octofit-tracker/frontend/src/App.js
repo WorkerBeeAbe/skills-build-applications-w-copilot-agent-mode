df08c7389fecd0c20fc601fe1ed2a54ea151ba20
WorkerBeeAbe <126785184+WorkerBeeAbe@users.noreply.github.com>
2025-04-08 12:05:38 +0000

    stage 5-2

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Activities from './Activities';
+import Leaderboard from './Leaderboard';
+import Teams from './Teams';
+import Users from './Users';
+import Workouts from './Workouts';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Router>
+      <div className="App">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">OctoFit Tracker</a>
+            <Link className="navbar-brand" to="/">OctoFit Tracker</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">Home</a>
+                  <Link className="nav-link" to="/activities">Activities</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Features</a>
+                  <Link className="nav-link" to="/leaderboard">Leaderboard</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Pricing</a>
+                  <Link className="nav-link" to="/teams">Teams</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/users">Users</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/workouts">Workouts</Link>
                 </li>
               </ul>
             </div>
@@ -28,78 +40,17 @@ function App() {
         </nav>
 
         <div className="container mt-4">
-          <h1 className="display-4">Welcome to OctoFit Tracker</h1>
-          <p className="lead">Track your fitness activities and compete with others!</p>
-
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Activity Log</h5>
-              <p className="card-text">Keep track of your daily fitness activities.</p>
-              <button className="btn btn-primary">View Activities</button>
-            </div>
-          </div>
-
-          <table className="table mt-4">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Activity</th>
-                <th scope="col">Duration</th>
-                <th scope="col">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Running</td>
-                <td>30 mins</td>
-                <td><button className="btn btn-danger">Delete</button></td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Swimming</td>
-                <td>1 hour</td>
-                <td><button className="btn btn-danger">Delete</button></td>
-              </tr>
-            </tbody>
-          </table>
-
-          <form className="mt-4">
-            <div className="mb-3">
-              <label htmlFor="activityName" className="form-label">Activity Name</label>
-              <input type="text" className="form-control" id="activityName" placeholder="Enter activity name" />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="duration" className="form-label">Duration</label>
-              <input type="text" className="form-control" id="duration" placeholder="Enter duration" />
-            </div>
-            <button type="submit" className="btn btn-success">Add Activity</button>
-          </form>
-
-          <button type="button" className="btn btn-info mt-4" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Open Modal
-          </button>
-
-          <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div className="modal-body">
-                  This is a Bootstrap modal example.
-                </div>
-                <div className="modal-footer">
-                  <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                  <button type="button" className="btn btn-primary">Save changes</button>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Routes>
+            <Route path="/activities" element={<Activities />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
+            <Route path="/teams" element={<Teams />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/workouts" element={<Workouts />} />
+            <Route path="/" element={<h1>Welcome to OctoFit Tracker</h1>} />
+          </Routes>
         </div>
-      </header>
-    </div>
+      </div>
+    </Router>
   );
 }
 
